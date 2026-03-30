641e8833a33d20da43c6130235871563b522e1b1
Sariaka Razakandisa <sariakaraz8@gmail.com>
2026-03-30 12:07:09 +0300

    feat : formulaire de login avec les champs  suivants : - username - password

---
+++ pages/login/form.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Karakory - Connexion</title>
+    <link rel="stylesheet" href="../../assets/css/login.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-left">
+            <img src="../../assets/images/karakory-login.png" alt="Karakory Logo">
+        </div>
+        <div class="login-right">
+            <div class="login-form-wrapper">
+                <h1>Connexion</h1>
+                <form method="POST" action="" class="login-form">
+                    <div class="form-group">
+                        <label for="username">Nom d'utilisateur</label>
+                        <input type="text" id="username" name="username" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Mot de passe</label>
+                        <input type="password" id="password" name="password" required>
+                    </div>
+                    <button type="submit" class="btn-login">Se connecter</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
