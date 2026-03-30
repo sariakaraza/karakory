6e6c022ef6509bce7d2839f5f4afaf7fc69756c9
Sandratra Rabenanahary <sandratrahasinarivelo5@gmail.com>
2026-03-30 13:22:14 +0300

    feat : ajout des donnees

---
+++ sql/data.sql
@@ -0,0 +1,9 @@
+-- Insertion de 5 utilisateurs pour la table `users`
+-- Mot de passe stocké en clair ici pour faciliter les tests (adapter en production)
+INSERT INTO users (username, email, mot_de_passe, role) VALUES
+('rakoto', 'rakoto@gmail.com', 'rakotoAdmin', 'admin'),
+('rasoa', 'rasoa@gmail.com', 'rasoaAdmin', 'admin'),
+('rabe', 'rabe@gmail.com', 'rabe123', 'user'),
+('razaka', 'razaka@gmail.com', 'razaka456', 'user'),
+('ratavy', 'ratavy@gmail.com', 'ratavy789', 'user');
+
